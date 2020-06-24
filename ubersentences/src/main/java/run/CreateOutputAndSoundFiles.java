8ec37153a34a24a7d011592f0ea3c0de23ab693c
dude <go@suckadick.com>
2020-06-24 09:26:41 +0200

    quick commit

---
+++ ubersentences/src/main/java/run/CreateOutputAndSoundFiles.java
@@ -0,0 +1,13 @@
+package run;
+
+import core.CsvProcessor;
+
+public class CreateOutputAndSoundFiles {
+
+    public static void main(final String[] args) {
+
+        new CsvProcessor().run();
+
+    }
+
+}
