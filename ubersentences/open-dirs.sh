b87373b143c1b53fd3370aac78a6f7fd06e0f4b6
dude <go@suckadick.com>
2020-12-04 23:34:31 +0100

    quick commit

---
+++ ubersentences/open-dirs.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+nautilus "/home/cc/Desktop/Programming/Repos/jp/ubersentences/anki/ubersentences/mp3/anime1"
+nautilus "/home/cc/.local/share/Anki2/User 1/collection.media/ubersentences/mp3/anime1"
+nautilus "/run/user/1000/gvfs/mtp:host=MediaTek_Lenovo_0123456789ABCDEF/Internal storage/AnkiDroid/collection.media/ubersentences/mp3/anime1"
